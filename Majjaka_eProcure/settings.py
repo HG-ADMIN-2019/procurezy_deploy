dfe819ace1960e5bec72ebe273e090983d116ce2
shilpa <shilpa.ilkal@hiranya-garbha.com>
2023-11-17 14:31:20 +0530

    db change

--- Majjaka_eProcure/settings.py
+++ Majjaka_eProcure/settings.py
@@ -162,7 +162,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # Database engine
         'HOST': 'sg2plzcpnl489587.prod.sin2.secureserver.net',  # Database host
-        'NAME': 'MEP_MIGRATE_DEV',
+        'NAME': 'MEP_DEPLOY',
         # Database name MEP_MIGRATE_DEV MEP_FIXING MEP_DEPLOYED_NEW_QA_23 MEP_DEPLOYED_NEW_29 MEP_DEPLOYED_NEW_QA_0506
         'USER': 'MajjakaShopEProcure',  # Database username
         'PASSWORD': 'Project@2019',  # Database credentialsgi
