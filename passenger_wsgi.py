b9a145b8472ca8f083ca55e2930398c6739d6b7f
HG-ADMIN-2019 <lavanya@hiranya-garbha.com>
2023-11-16 19:55:26 -0700

    changes

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,22 +1,9 @@
-import Majjaka_eProcure.wsgi
-SCRIPT_NAME = 'home/a98zy7m9c1gd/repositories/Majjaka-Deployment'
+import imp
+import os
+import sys
 
-class PassengerPathInfoFix(object):
-    """
-    Sets PATH_INFO from REQUEST_URI because Passenger doesn't provide it.
-    """
-    def __init__(self, app):
-        self.app = app
 
-    def __call__(self, environ, start_response):
-        from urllib.parse import unquote
-        environ['SCRIPT_NAME'] = SCRIPT_NAME
+sys.path.insert(0, os.path.dirname(__file__))
 
-        request_uri = unquote(environ['REQUEST_URI'])
-        script_name = unquote(environ.get('SCRIPT_NAME', ''))
-        offset = request_uri.startswith(script_name) and len(environ['SCRIPT_NAME']) or 0
-        environ['PATH_INFO'] = request_uri[offset:].split('?', 1)[0]
-        return self.app(environ, start_response)
-
-application = Majjaka_eProcure.wsgi.application
-application = PassengerPathInfoFix(application)
+wsgi = imp.load_source('wsgi', 'passenger_wsgi.py')
+application = wsgi.application
