fbedffb04c1535dc998e66be2e69b9db83d2896d
Siddharth1097 <siddarth@majjaka.com>
2023-12-13 14:59:44 +0530

    fix

--- Majjaka_eProcure/settings.py
+++ Majjaka_eProcure/settings.py
@@ -12,10 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 # import django_heroku
 import os
-os.environ['DISPLAY'] = ':0'
-os.environ['XAUTHORITY']='/run/user/1000/gdm/Xauthority'
-import pyautogui
-print(pyautogui.position())
+
 # import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
